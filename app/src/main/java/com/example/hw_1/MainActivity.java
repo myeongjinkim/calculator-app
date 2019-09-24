bf21348a839077fad551443ff7745ff144bd0d19
60142244_김명진 <kiakd2@naver.com>
2019-09-24 15:22:06 +0900

    create button press

--- app/src/main/java/com/example/hw_1/MainActivity.java
+++ app/src/main/java/com/example/hw_1/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.hw_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView textView1 = (TextView) findViewById(R.id.textView1) ;
+        textView1.setText("Text1 is changed.") ;
+        TextView textView2 = (TextView) findViewById(R.id.textView2) ;
+        textView2.setText("Text2 is changed.") ;
+
+    }
+    public void pressButton(View view)
+    {
+        System.out.println("Press");
     }
 }
