63f95a17aafbb03e62223d133de0ae21b3d0c7cd
60142244_김명진 <kiakd2@naver.com>
2019-09-24 15:26:57 +0900

    action button listen

--- app/src/main/java/com/example/hw_1/MainActivity.java
+++ app/src/main/java/com/example/hw_1/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
     }
     public void pressButton(View view)
     {
-        System.out.println("Press");
+        String s = (String) ((Button) view).getText();
+        System.out.println("Press : " + s);
+
     }
 }
